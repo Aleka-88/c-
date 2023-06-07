168e7c97f34bc0fae613396771a8809d49ee0497
agapova <agapova@gmail.com>
2023-06-07 15:02:31 +0400

    done

---
+++ семинар_3/семинар_3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Урок 3. Базовые алгоритмы. Продолжение
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+/*Console.Write("ведите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=10000 && number <=99999)
+{Console.Write("пятизначное число");
+int firstDigit =number/10000;
+int twostDigit =number/1000%10;
+int fourstDigit =number/10;
+int fivestDigit =number/10%10;
+
+if (firstDigit ==fivestDigit && twostDigit==fourstDigit)
+
+Console.WriteLine("палиндромом");}
+
+else 
+Console.WriteLine(" НЕ палиндромом");
+/*
+
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+/*Console.Write("ведите число :");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=N;i=i+1
+)
+{
+Console.WriteLine(Math.Pow(i,3)+"\t");
+
+}
+*/
+
+
+
+  
