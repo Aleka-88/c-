a2184ed02f708dcb7a4d1b846efa410e043c1e91
agapova <agapova@gmail.com>
2023-06-14 14:58:49 +0400

    test

---
+++ семинар_5/семинар_5/Program.cs
@@ -0,0 +1,87 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+/*int size = 5;
+int[] array = new int [size]; 
+Random rand = new Random();
+for (int i = 0; i < size; i++)
+{
+array[i] = rand.Next(100,1000);
+if (array[i] % 2 == 0)
+{
+    Console.WriteLine($"{array[i]} - Четное");
+}
+else
+
+Console.WriteLine($"{array[i]} - Нечетное");
+}
+*/
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+/*
+int size=5;
+int[] array = new int [size]; 
+Random rand = new Random();
+int sum = 0;
+
+for (int i = 0; i < size; i++)
+{
+array[i] = rand.Next(100,1000);
+if ( i % 2!=0)
+{
+sum = sum + array[i];
+}
+
+}Console.WriteLine($"Созданный массив:[{string.Join("; ",array)}]");
+
+Console.WriteLine($"Сумма  {sum}");
+/*
+
+/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+*/
+int size;
+Console.Write("Введите кол-во элементов: ");
+size = Convert.ToInt32(Console.ReadLine()); // считываем размер массива
+double [] array = new double [size];
+Random rand = new Random();
+void GetArray(double[] array) // метод заполнения массива 
+{
+     for (int i = 0; i < size; i++)
+     array[i] = Math.Round(rand.NextDouble()*10, 2);
+}  
+
+GetArray(array); 
+Console.WriteLine($"Созданный массив:[{string.Join("; ",array)}]"); 
+
+double dev = 0;
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < size; i++) 
+{ 
+  if(array[i] < min) min = array[i];
+
+  if (array[i] > max) max = array[i];
+} 
+dev = Math.Round(max - min, 2);
+
+Console.WriteLine($"Мин {min} Макс {max} Разница  {dev}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ семинар_7/семинар_7/Program.cs
@@ -0,0 +1,124 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+// Двумерный массив
+
+// метод для создания массива 
+// Двумерный массив = matrix
+// m - кол-во строчек, n - столбцов
+
+/*Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);*/
+
+
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+/*Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Random rnd = new Random();
+void PrintArray(int[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+
+PrintArray(array);
+System.Console.WriteLine();
+Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ {if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
+}
+*/
+
+
+
+/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// m - кол-во строчек, n - столбцов
+*/
+
+/*Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Random rnd = new Random();
+void PrintArray(int[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+
+PrintArray(array);
+System.Console.WriteLine();
+Console.WriteLine("среднее арифметическое элементов в каждом столбце");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i,j]);
+    }
+    avarage = avarage / m;
+    Console.Write(avarage + "; ");
+    
+}
+*/
