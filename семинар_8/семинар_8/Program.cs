2fa8a8c6615d41724f848469dc76128c21abe05b
agapova <agapova@gmail.com>
2023-06-20 12:28:08 +0400

    test

---
+++ семинар_8/семинар_8/Program.cs
@@ -0,0 +1,140 @@
+﻿/*Урок 8. Двумерные массивы. Продолжение
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+// метод для создания массива 
+// Двумерный массив = matrix
+// m - кол-во строчек, n - столбцов
+// Функция заполнения массива рандомно числами от 1 до 10
+
+/*int[,] table = new int[3, 4];
+FillArrayRandom(table);
+PrintArray(table);
+SortToLower(table);
+Console.WriteLine();
+PrintArray(table);
+
+
+// Функция заполнения массива рандомно числами от 1 до 9
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    /*
+    
+
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+/*int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n]; // Таблица из m строк и n столбцов 
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам , i < m
+    {
+        // i,j,m,k 
+        for (int j = 0; j < matrix.GetLength(1); j++)// Цикл по стобцам , j < n
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Метод, который печатает массив 
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write(array[i, j] + "\t"); // "\t" = Tab = 4 пробела между элементами
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetRowNumber(int[,] matrix)
+{
+    int rowWithMinSum = 0;
+    int minSum = 0;
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minSum = minSum + matrix[0, i]; // minSum = сумме элементов 0 строчки
+    }
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j]; // сумма элементов текущей строчки, начиная от 1
+        }
+        if (sum < minSum)
+        {
+            minSum = sum; // Если сумма текущей строчки меньше предп-й мин. суммы, то мы
+            // перезаписываем переменную
+            rowWithMinSum = i;
+        }
+    }
+    return rowWithMinSum;
+}
+
+int[,] matr = GetMatrix(3, 5, 0, 10); // Матрица 5 на 5, элемент - рандомное число от 0 до 10 вклю-то
+PrintMatrix(matr);
+
+Console.WriteLine($"Строчка с наименьшей суммой: {GetRowNumber(matr)}");
+*/
